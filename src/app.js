fcf60a6a244505461458e7e279798185de6a14a9
Marcio-VOT <marciofsantos65@gmail.com>
2023-01-16 08:50:57 -0300

    err IX

--- src/app.js
+++ src/app.js
@@ -37,13 +37,12 @@ app.post('/participants', async (req,res)=>{
 
     try {
         const validation = schema.validate({name}, { abortEarly: false })
-        console.log(validation);
         if(validation.error){
             return res.sendStatus(422);
         }
         const usedName = await db.collection("participants").findOne({name});
         if(usedName != null){
-            return res.status(422).send("babalu")}
+            return res.status(409)}
         await db.collection("participants").insertOne({name, lastStatus : Date.now()});
         db.collection("messages").insertOne({from:name, to:'Todos', text: 'entra na sala...', type: 'status', time: dayjs().format('HH:mm:ss') })
         res.sendStatus(201);
@@ -67,7 +66,7 @@ app.post('/messages', async (req, res)=>{
     const schema = joi.object({
         to : joi.string().required(),
         text: joi.string().required(),
-        type: joi.string().valid('message','private_message'),
+        type: joi.string().valid('message','private_message').required(),
     })
     try {   
         const validation = schema.validate({to, text, type});
@@ -87,14 +86,22 @@ app.get('/messages/:limit?', async (req, res)=>{
     const { to, text, type} = req.body;
     const user = req.headers.user;
     const limit = req.params.limit  
+
+    const schema = joi.object({
+        limit: joi.number().positive().min(1)
+    })
     
     try {   
         //let temp = await db.collection("messages").insertOne({ from: user, to, text, type, time: dayjs().format('HH:mm:ss')});
         let messagesList = await db.collection("messages").find({}).toArray();
 
         messagesList = messagesList.filter((msg)=> (msg.from === user || msg.to === user || msg.type === "message" || msg.type === "status"))
-        if(limit && limit >= 0){
-            return res.send(messagesList.slice(-(limit)));
+        const verification = schema.validate({limit});
+        
+        if(verification.error){
+            return res.sendStatus(422);
+        }else if(limit){
+        return res.send(messagesList.slice(-(limit)));
         }
         res.send(messagesList);
     } catch (error) {
